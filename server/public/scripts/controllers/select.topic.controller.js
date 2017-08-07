82ab2433c43b30bbba40bd34a6bdfcf3a5d75c0e
Alec Sands <sandsalecv@gmail.com>
2017-08-07 15:55:34 -0500

    Added a prompt for a new topic

--- server/public/scripts/controllers/select.topic.controller.js
+++ server/public/scripts/controllers/select.topic.controller.js
@@ -1,6 +1,26 @@
-myApp.controller('SelectTopicController', function(IdeaShuffleService) {
+myApp.controller('SelectTopicController', function(IdeaShuffleService, $mdDialog) {
   console.log('SelectTopicController created');
   var vm = this;
   vm.userService = IdeaShuffleService;
   vm.userObject = IdeaShuffleService.userObject;
+  vm.test = function() {
+    console.log('Prompting for new topic');
+  };
+  vm.showPrompt = function(ev) {
+   // Appending dialog to document.body to cover sidenav in docs app
+   var confirm = $mdDialog.prompt()
+     .title('What is the new topic?')
+     .placeholder('Topic Title')
+     .ariaLabel('Topic Title')
+     .initialValue('Topic')
+     .targetEvent(ev)
+     .ok('Create')
+     .cancel('Cancel');
+
+   $mdDialog.show(confirm).then(function(result) {
+     console.log('Created a new topic');
+   }, function() {
+     console.log('Canceled creating a new topic');
+   });
+ };
 });
