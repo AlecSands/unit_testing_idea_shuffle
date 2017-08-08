22bcc7bc20cf1bdf17474e438c423d0b8c11b3b7
Alec Sands <sandsalecv@gmail.com>
2017-08-08 14:09:22 -0500

    implemented basic ng-sortable

--- server/public/scripts/controllers/current.topic.controller.js
+++ server/public/scripts/controllers/current.topic.controller.js
@@ -2,4 +2,20 @@ myApp.controller('CurrentTopicController', function(IdeaShuffleService) {
   console.log('CurrentTopicController created');
   var vm = this;
   vm.userService = IdeaShuffleService;
+
+  vm.dragControlListeners = {
+    // accept: function (sourceItemHandleScope, destSortableScope) {return boolean;}, //override to determine drag is allowed or not. default is true.
+    itemMoved: function (event) {
+      console.log('in item moved:', vm.categories1, vm.categories2);
+    },
+    orderChanged: function(event) {
+      console.log('changing order', vm.categories1, vm.categories2);
+    },
+    containment: '#drag-containment', //optional param.
+    clone: false, //optional param for clone feature.
+    allowDuplicates: true, //optional param allows duplicates to be dropped.
+  };
+
+  vm.categories1 = ['test1', 'test2', 'test with longer text'];
+  vm.categories2 = ['test with some stuff', 'testing', 'test with longer text for fun'];
 });
